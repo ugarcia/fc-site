3c29b1b0d6990752103ea7e822d3858a6dbcd18d
Unai <unai.gg77@gmail.com>
2014-06-04 21:46:19 +0200

    timeline update

--- js/bio.js
+++ js/bio.js
@@ -4,7 +4,7 @@ var timeline_config = {
 	width: "100%",
 	height: "100%",
 	source: 'json/timeline.jsonp',
-    start_zoom_adjust:  '3',
+    start_zoom_adjust:  '5',
     css: 'css/timeline.css',     //OPTIONAL PATH TO CSS
 	js: 'js/timeline-min.js'    //OPTIONAL PATH TO JS
 };
